096325aab98abd9015b728975a41d0cf812ab9ac
sergeyBreslavets <venom.bren@gmail.com>
2015-09-09 17:09:25 +0400

    add file for start project

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+'use strict';
+
+// include gulp
+var gulp = require('gulp'); 
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+var minifyHTML = require('gulp-minify-html');
+
+
+// include plug-ins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('./src/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+// minify new images
+gulp.task('imagemin', function() {
+  var imgSrc = './src/images/**/*',
+      imgDst = './build/images';
+
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+  var htmlSrc = './src/*.html',
+      htmlDst = './build';
+
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+  gulp.src(['./src/scripts/lib.js','./src/scripts/*.js'])
+    .pipe(concat('script.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts/'));
+});
+// CSS concat, auto-prefix and minify
+gulp.task('styles', function() {
+  gulp.src(['./src/styles/*.css'])
+    .pipe(concat('styles.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/styles/'));
+});
+// default gulp task
+
+gulp.task('default', ['imagemin', 'htmlpage', 'scripts', 'styles', 'sass'], function() {
+  // watch for HTML changes
+  gulp.watch('./src/*.html', function() {
+    gulp.run('htmlpage');
+  });
+
+  // watch for JS changes
+  gulp.watch('./src/scripts/*.js', function() {
+    gulp.run('jshint', 'scripts');
+  });
+
+  // watch for CSS changes
+  gulp.watch('./src/styles/*.css', function() {
+    gulp.run('styles');
+  });
+});
+
+/*
+gulp.task('sass', function() {
+    gulp.src('./src/sass/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/styles'))
+});*/
+
+gulp.task('sass', function() {
+  gulp.src('./src/sass/{,*/}*.{scss,sass}')
+   /*.pipe(sourcemaps.init())*/
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+   // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/styles'));
+});
